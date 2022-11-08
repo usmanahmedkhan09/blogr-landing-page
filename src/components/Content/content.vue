a59e713360440340ce16270137fdd50e65a317b0
usman ahmed khan <usmanahmedkhan@live.com>
2022-11-08 12:08:51 +0500

    new images added in section two

--- src/components/Content/content.vue
+++ src/components/Content/content.vue
@@ -1,14 +1,16 @@
 <template>
   <main class="content">
     <SectionOne />
+    <SectionTwo />
   </main>
 </template>
 <script lang="ts">
 import { defineComponent } from "vue";
-import SectionOne from "./sections/sectionOneComponenet.vue";
+import SectionOne from "@/components/Content/sections/sectionOneComponent.vue";
+import SectionTwo from "@/components/Content/sections/sectionTwoComponent.vue";
 
 export default defineComponent({
-  components: { SectionOne },
+  components: { SectionOne, SectionTwo },
   setup() {
     return {};
   },
---
+++ src/components/Content/sections/sectionTwoComponent.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="section--two">
+    <div class="icons_wrapper">
+      <img
+        src="../../../assets/images/illustration-phones.svg"
+        alt=""
+        srcset=""
+      />
+      <!-- <PhonesIcon /> -->
+    </div>
+    <div class="description_wrapper">
+      <h3>State of the Art infrastructure</h3>
+      <p>
+        With reliabilitiy and speed in mind, worldwide data centers provide the
+        backbone for ultra fast connectivity.This ensures your site will load
+        instantly, no matters where your reader are, keeping your site
+        competitive.
+      </p>
+    </div>
+  </div>
+</template>
+<script lang="ts">
+import { defineComponent } from "vue";
+import PhonesIcon from "../../Icons/phones.vue";
+
+export default defineComponent({
+  components: { PhonesIcon },
+  setup() {
+    return {};
+  },
+});
+</script>
