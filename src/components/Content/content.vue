312621d5e060849e65614517b2f49a9953cfb4bc
usman ahmed khan <usmanahmedkhan@live.com>
2022-11-08 12:40:37 +0500

    added anthor section in content

--- src/components/Content/content.vue
+++ src/components/Content/content.vue
@@ -2,15 +2,17 @@
   <main class="content">
     <SectionOne />
     <SectionTwo />
+    <sectionThree />
   </main>
 </template>
 <script lang="ts">
 import { defineComponent } from "vue";
 import SectionOne from "@/components/Content/sections/sectionOneComponent.vue";
 import SectionTwo from "@/components/Content/sections/sectionTwoComponent.vue";
+import sectionThree from "@/components/Content/sections/sectionThreeComponent.vue";
 
 export default defineComponent({
-  components: { SectionOne, SectionTwo },
+  components: { SectionOne, SectionTwo, sectionThree },
   setup() {
     return {};
   },
---
+++ src/components/Content/sections/sectionThreeComponent.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="section--three">
+    <div class="description_wrapper">
+      <div class="editior-icon">
+        <img
+          src="../../../assets/images/illustration-laptop-desktop.svg"
+          alt="editior"
+        />
+      </div>
+      <div class="des_content">
+        <div class="description">
+          <h4>Free, open, simple</h4>
+          <p>
+            Bloger is free and open source application backed by a large
+            communityof helpful developers.It support features such as code
+            syntax highlighting.RSS feeds, social media integration,
+            third-party, commenting tools, and works seamlessly with google
+            Analytics.The architecture is clean and is relatively easy to learn.
+          </p>
+        </div>
+        <div class="description">
+          <h4>Powerful tooling</h4>
+          <p>
+            Battries included.We build a simple and straightfowrad CLI tool that
+            makes customization and delpoyment a breeze, but caplable of
+            producing even the most complicated sites
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  setup() {
+    return {};
+  },
+});
+</script>
