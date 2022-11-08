7c450d85f781f685f34137f50f29b763d5e8d097
usman ahmed khan <usmanahmedkhan@live.com>
2022-11-08 15:24:38 +0500

    footer added

---
+++ src/components/footer.vue
@@ -0,0 +1,43 @@
+<template>
+  <footer class="footer">
+    <div class="logo_wrapper">
+      <img src="../assets/images/logo.svg" alt="logo" />
+    </div>
+    <div class="list_wrapper">
+      <p class="title">Product</p>
+      <ul>
+        <li>Overview</li>
+        <li>Pricing</li>
+        <li>MarketPlace</li>
+        <li>Features</li>
+        <li>Integrations</li>
+      </ul>
+    </div>
+    <div class="list_wrapper">
+      <p class="title">Company</p>
+      <ul>
+        <li>About</li>
+        <li>Team</li>
+        <li>Blog</li>
+        <li>Careers</li>
+      </ul>
+    </div>
+    <div class="list_wrapper">
+      <p class="title">Content</p>
+      <ul>
+        <li>Context</li>
+        <li>Newsletter</li>
+        <li>linkedin</li>
+      </ul>
+    </div>
+  </footer>
+</template>
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  setup() {
+    return {};
+  },
+});
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,14 +1,16 @@
 <template>
   <Header />
   <Content />
+  <Footer />
 </template>
 <script lang="ts">
 import { defineComponent } from "vue";
 import Header from "@/components/header.vue";
 import Content from "@/components/Content/content.vue";
+import Footer from "@/components/footer.vue";
 
 export default defineComponent({
-  components: { Header, Content },
+  components: { Header, Content, Footer },
   setup() {
     return {};
   },
